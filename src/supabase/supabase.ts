748dcbf32eaf5295e9e31637b88d2389bd461e97
Sigrunnu <sigrun.nummedal@gmail.com>
2025-03-08 13:57:13 +0100

    Delete unusded supabase files

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl =  "https://mvfgovwbkpojurkplimu.supabase.co"
-const supabaseKey = process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey!)
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12Zmdvdndia3BvanVya3BsaW11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MzEzODYsImV4cCI6MjA1NzAwNzM4Nn0.NrKBQt1tWNh7seAZjbBmdcWzV-dVhwx6A1XVFOIXjgo"
+export const supabase = createClient(supabaseUrl, supabaseKey)
