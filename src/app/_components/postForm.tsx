a46ca2953f5daf3b0bbfce80db3c1990f0a0e0cb
Sigrunnu <sigrun.nummedal@gmail.com>
2025-03-09 13:51:33 +0100

    Laga eit modal-form som ser heilt ok ut

--- src/app/_components/postForm.tsx
+++ src/app/_components/postForm.tsx
@@ -1,19 +1,24 @@
 "use client";
+import Add from "@mui/icons-material/Add";
 
 import { FormEvent, useState } from "react";
 
 import { api } from "~/trpc/react";
 import { uploadImage } from "~/supabase/image-service";
+import { Dialog } from "@mui/material";
 
 export function PostForm() {
   const utils = api.useUtils();
   const [name, setName] = useState("");
   const [selectedFile, setSelectedFile] = useState<File | null>();
+  const [openModal, setOpenModal] = useState(false);
 
   const createPost = api.post.create.useMutation({
     onSuccess: async () => {
       await utils.post.invalidate();
       setName("");
+      setOpenModal(false);
+      setSelectedFile(null);
     },
   });
 
@@ -27,34 +32,63 @@ export function PostForm() {
   };
 
   return (
-    <div className="w-full max-w-xs py-2">
-      <form onSubmit={submit} className="flex flex-col gap-2">
-        <input
-          type="text"
-          placeholder="Title"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="w-full rounded-full px-4 py-2 text-black"
-        />
-        <input
-          className={
-            "rounded-full bg-white/20 px-10 py-3 font-semibold transition hover:bg-white/30"
-          }
-          type="file"
-          onChange={(event) =>
-            setSelectedFile(event.target.files ? event.target.files[0] : null)
-          }
-        />
-        {/*Bruk "environmen for å åpne kamerarull direkte. Kanskje bruke kun den om ein er på mobil?*/}
-        {/*<input accept="image/*" id="icon-button-file" type="file" capture="environment"/>*/}
-        <button
-          type="submit"
-          className="rounded-full bg-white/20 px-10 py-3 font-semibold transition hover:bg-white/30"
-          disabled={createPost.isPending}
+    <div className="flex w-full max-w-xs flex-col items-center py-2">
+      <button
+        onClick={() => setOpenModal(true)}
+        className="opacity my-3 flex items-center rounded-xl bg-white/40 px-4 py-3 transition hover:bg-white/30"
+      >
+        <Add />
+      </button>
+      {openModal && (
+        <Dialog
+          open={openModal}
+          onClose={() => setOpenModal(false)}
+          className="relative z-10"
         >
-          {createPost.isPending ? "Submitting..." : "Submit"}
-        </button>
-      </form>
+          <div
+            className="relative z-10"
+            aria-labelledby="modal-title"
+            role="dialog"
+            aria-modal="true"
+          >
+            <div>
+              <p className={"px-2 py-2 text-lg font-bold text-[#f08080]"}>
+                Lag ny post
+              </p>
+              <form
+                onSubmit={submit}
+                className="flex flex-col items-center gap-2 p-4"
+              >
+                <input
+                  type="text"
+                  placeholder="Title"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="w-full rounded-xl border-2 border-gray-200 p-2"
+                />
+                <input
+                  className={
+                    "w-full rounded-xl border-2 border-gray-200 p-2 px-4 text-black"
+                  }
+                  type="file"
+                  onChange={(event) =>
+                    setSelectedFile(
+                      event.target.files ? event.target.files[0] : null,
+                    )
+                  }
+                />
+                <button
+                  type="submit"
+                  className="w-20 rounded-xl bg-[#f08080]/50 py-2 font-semibold transition hover:bg-[#f08080]/30"
+                  disabled={createPost.isPending}
+                >
+                  {createPost.isPending ? "Submitting..." : "Submit"}
+                </button>
+              </form>
+            </div>
+          </div>
+        </Dialog>
+      )}
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen bg-gradient-to-b from-[#f6c492] to-[#f6b092] text-white">
+      <main className="flex min-h-screen justify-center bg-gradient-to-b from-[#f6c492] to-[#f6b092] text-white">
         <div className="container flex flex-col items-center justify-between px-4">
           <div className={"flex flex-col items-center"}>
             <Image
